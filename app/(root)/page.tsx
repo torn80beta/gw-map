aee1c7c48166e95795b69b6dd1098ddb77e4f6f0
Alex <103459735+torn80beta@users.noreply.github.com>
2023-09-17 12:06:18 +0300

    adaptive paddings & font size

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,5 +4,10 @@ import { currentUser } from "@clerk/nextjs";
 export default async function Home() {
   const user = await currentUser();
   // console.log(user);
-  return <main className="page-container">Home page</main>;
+  return (
+    <main className="page-container">
+      <div className="">test</div>
+      <p>Home page</p>
+    </main>
+  );
 }
