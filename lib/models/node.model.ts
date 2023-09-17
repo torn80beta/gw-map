779ea85b2736fa608c3f5bfcff424bb34aaa5fda
Alex <103459735+torn80beta@users.noreply.github.com>
2023-09-17 12:16:24 +0300

    nodeSchema

--- lib/models/node.model.ts
+++ lib/models/node.model.ts
@@ -1,15 +1,25 @@
 import mongoose from "mongoose";
 
 const nodeSchema = new mongoose.Schema({
-  tel1: {
+  street: {
     type: String,
     required: true,
+  },
+  building: {
+    type: String,
+    required: true,
+  },
+  tel1: {
+    type: String,
   },
   comment1: { type: String, required: true },
   tel2: {
     type: String,
   },
   comment2: { type: String },
+  fibers: {
+    type: [{ color: String, target: String }],
+  },
   author: {
     type: String,
     required: true,
