3459b77512a4640af9d5ff83cdad1e05eec27c12
Alex <103459735+torn80beta@users.noreply.github.com>
2023-09-22 18:17:11 +0300

    update fix

--- lib/actions/node.actions.ts
+++ lib/actions/node.actions.ts
@@ -189,9 +189,7 @@ export async function updateNode({
       throw new Error("Node not found");
     }
 
-    const node = await Node.findById(id);
-    // console.log(node);
-    revalidatePath(path);
+    const node = await Node.findById(id, "_id");
     return JSON.stringify({ ...node._doc, status: 204 });
   } catch (error) {
     console.error("Error updating node:", error);
@@ -252,44 +250,5 @@ export async function updateNode({
 //     revalidatePath(path);
 //   } catch (error: any) {
 //     throw new Error(`Failed to delete thread: ${error.message}`);
-//   }
-// }
-
-// export async function addCommentToThread(
-//   threadId: string,
-//   commentText: string,
-//   userId: string,
-//   path: string
-// ) {
-//   connectToDB();
-
-//   try {
-//     // Find the original thread by its ID
-//     const originalThread = await Thread.findById(threadId);
-
-//     if (!originalThread) {
-//       throw new Error("Thread not found");
-//     }
-
-//     // Create the new comment thread
-//     const commentThread = new Thread({
-//       text: commentText,
-//       author: userId,
-//       parentId: threadId, // Set the parentId to the original thread's ID
-//     });
-
-//     // Save the comment thread to the database
-//     const savedCommentThread = await commentThread.save();
-
-//     // Add the comment thread's ID to the original thread's children array
-//     originalThread.children.push(savedCommentThread._id);
-
-//     // Save the updated original thread to the database
-//     await originalThread.save();
-
-//     revalidatePath(path);
-//   } catch (err) {
-//     console.error("Error while adding comment:", err);
-//     throw new Error("Unable to add comment");
 //   }
 // }
