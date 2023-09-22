b4961d58815e1903ed207c5ea6e2b83eee12b0c5
Alex <103459735+torn80beta@users.noreply.github.com>
2023-09-22 18:12:00 +0300

    links

--- components/shared/BurgerMenu/BurgerMenu.tsx
+++ components/shared/BurgerMenu/BurgerMenu.tsx
@@ -15,28 +15,12 @@ function BurgerMenu() {
     <div className="md:hidden">
       <Sheet>
         <SheetTrigger>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-            />
-          </svg>
+          <Image src="/assets/burger.svg" alt="menu" width={24} height={24} />
         </SheetTrigger>
         <SheetContent side="left">
           <SheetHeader>
             <SheetTitle>Useful links</SheetTitle>
-            <SheetDescription>
-              {/* This action cannot be undone. This will permanently delete your
-              account and remove your data from our servers. */}
-            </SheetDescription>
+            <SheetDescription></SheetDescription>
           </SheetHeader>
           <div className="w-full flex flex-col gap-6 py-8 text-xl">
             {burgerLinks.map((link) => (
@@ -46,7 +30,7 @@ function BurgerMenu() {
                 rel="noopener noreferrer"
                 target="_blank"
               >
-                <div className="h-6 flex row  gap-3">
+                <div className="h-6 flex row gap-3">
                   <Image
                     src={link.imgURL}
                     alt={link.label}
